0cd0f94e56458902c28b97efc4dc83cb4860abe7
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-03-13 16:22:13 +0800

    feat: 新增 Shadcn 按鈕元件

--- apps/website1/app.vue
+++ apps/website1/app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     Website one
-    <nx3-btn> smile 1 </nx3-btn>
+    <nx3-btn>客製化元件庫元件</nx3-btn>
+    <Button>本地元件</Button>
+    <nx3-Button>Shadcn-vue 元件庫元件</nx3-Button>
   </div>
 </template>
